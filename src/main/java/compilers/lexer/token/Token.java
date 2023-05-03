afcb0bddb5bd51ce49c3a0d0017b8d39f6b4863b
chabanyukm <chabanyuk.maks@gmail.com>
2023-05-03 07:12:37 +0300

    isValid property added

--- src/main/java/compilers/lexer/token/Token.java
+++ src/main/java/compilers/lexer/token/Token.java
@@ -5,11 +5,25 @@ public abstract class Token {
     protected final String type;
     protected final Object value;
     protected final String extra;
+    protected final boolean isValid;
 
-    public Token(String type, Object value, String extra) {
+    public Token(String type, Object value, String extra, boolean isValid) {
         this.type = type;
         this.value = value;
         this.extra = extra;
+        this.isValid = isValid;
+    }
+
+    public Token(String type, Object value, String extra) {
+        this(type, value, extra, true);
+    }
+
+    public Token(String type, Object value) {
+        this(type, value, "", true);
+    }
+
+    public boolean isValid() {
+        return isValid;
     }
 
     @Override
