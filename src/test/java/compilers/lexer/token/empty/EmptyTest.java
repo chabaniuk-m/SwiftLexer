ab1e59e67e3f95d899950d8779ae8841a33888cf
chabanyukm <chabanyuk.maks@gmail.com>
2023-05-04 09:44:04 +0300

    tests for all Whitespace methods

--- src/test/java/compilers/lexer/token/empty/EmptyTest.java
+++ src/test/java/compilers/lexer/token/empty/EmptyTest.java
@@ -11,7 +11,8 @@ import static org.junit.jupiter.api.Assertions.*;
 public class EmptyTest extends LexerTest {
 
     @Test
-    public void testSingleWhitespaceCharacter() {
+    public void singleWhitespaceCharacter() {
+        printTitle("single whitespace character");
         // space
         String code = " ";
         AtomicInteger index = new AtomicInteger(0);
@@ -59,8 +60,117 @@ public class EmptyTest extends LexerTest {
     }
 
     @Test
-    public void testCombinedWhitespaceSequence() {
-        String code = readTestData("empty/test1");
-        System.out.println("<" + code + ">");
+    public void sequenceOfWhiteSpaceCharacters() {
+        printTitle("sequence of whitespace characters");
+        // three spaces
+        String code = "   ";
+        AtomicInteger index = new AtomicInteger(0);
+        Token token = Whitespace.of(code, index);
+        assertEquals(new Token("WHITESPACE_SEQUENCE", "\"\\s\\s\\s\"", "", true),
+                token, "incorrect whitespace sequence processing");
+        assertEquals(3, index.get(), "function must properly increase the index");
+        System.out.println(token);
+
+        // combined
+        code = "let \t\f\r  \t x: Int ";
+        index.set(code.indexOf(' '));
+        token = Whitespace.of(code, index);
+        assertEquals(new Token("WHITESPACE_SEQUENCE", "\"\\s\\t\\f\\r\\s\\s\\t\\s\"", "", true),
+                token, "incorrect whitespace sequence processing");
+        assertEquals(code.indexOf('x'), index.get(), "function must properly increase the index");
+        System.out.println(token);
+
+        // combined till end
+        code = "return 1;\t\t\r" + (char)11 + "  \f ";
+        index.set(code.indexOf(';') + 1);
+
+        token = Whitespace.of(code, index);
+        assertEquals(new Token("WHITESPACE_SEQUENCE", "\"\\t\\t\\r\\v\\s\\s\\f\\s\"", "", true),
+                token, "incorrect whitespace sequence processing");
+        assertEquals(code.length(), index.get(), "function must properly increase the index");
+        System.out.println(token);
+    }
+
+    @Test
+    public void cloneWithoutLastSpace() {
+        printTitle("clone without last space");
+        // single space
+        String code = " ";
+        Token clone;
+        AtomicInteger index = new AtomicInteger(0);
+        Whitespace token = Whitespace.of(code, index);
+        assertNotNull(token);
+        assertNull(token.cloneWithoutLastSpace(), "single whitespace must convert into null");
+        System.out.println("null");
+
+        // 2 symbols
+        code = "\t ";
+        index.set(0);
+        token = Whitespace.of(code, index);
+        assertNotNull(token);
+        assertEquals("WHITESPACE_SYMBOL: '\\t' (tab)",
+                      (clone = token.cloneWithoutLastSpace()).toString(),
+                     "must remove only last space");
+        System.out.println(clone);
+
+        // 2 symbols
+        code = "\f ";
+        index.set(0);
+        token = Whitespace.of(code, index);
+        assertNotNull(token);
+        assertEquals("WHITESPACE_SYMBOL: '\\f' (form feed)",
+                      (clone = token.cloneWithoutLastSpace()).toString(),
+                     "must remove only last space");
+        System.out.println(clone);
+
+        // 2 symbols
+        code = "\r ";
+        index.set(0);
+        token = Whitespace.of(code, index);
+        assertNotNull(token);
+        assertEquals("WHITESPACE_SYMBOL: '\\r' (carriage return)",
+                      (clone = token.cloneWithoutLastSpace()).toString(),
+                     "must remove only last space");
+        System.out.println(clone);
+
+        // 2 symbols
+        code = (char)11 + " ";
+        index.set(0);
+        token = Whitespace.of(code, index);
+        assertNotNull(token);
+        assertEquals("WHITESPACE_SYMBOL: '\\v' (vertical tab)",
+                      (clone = token.cloneWithoutLastSpace()).toString(),
+                     "must remove only last space");
+        System.out.println(clone);
+
+        // 2 symbols
+        code = "  ";
+        index.set(0);
+        token = Whitespace.of(code, index);
+        assertNotNull(token);
+        assertEquals("WHITESPACE_SYMBOL: '\\s' (space)",
+                      (clone = token.cloneWithoutLastSpace()).toString(),
+                     "must remove only last space");
+        System.out.println(clone);
+
+        // several symbols
+        code = "let x \t\f\r\t ";
+        index.set(code.indexOf('x') + 1);
+        token = Whitespace.of(code, index);
+        assertNotNull(token);
+        assertEquals("WHITESPACE_SEQUENCE: \"\\s\\t\\f\\r\\t\"",
+                     (clone = token.cloneWithoutLastSpace()).toString(),
+                     "must remove only last space");
+        System.out.println(clone);
+
+        // several symbols with preceding space
+        code = " \t \f\r" + (char)11 + "\t  ";
+        index.set(0);
+        token = Whitespace.of(code, index);
+        assertNotNull(token);
+        assertEquals("WHITESPACE_SEQUENCE: \"\\s\\t\\s\\f\\r\\v\\t\\s\"",
+                     (clone = token.cloneWithoutLastSpace()).toString(),
+                     "must remove only last space");
+        System.out.println(clone);
     }
 }
